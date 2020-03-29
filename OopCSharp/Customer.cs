4e065a9a8f7005d412355704df1f9ffeba25292d
Agus-Novian <agusnovian21@gmail.com>
2020-03-29 22:47:45 +0700

    Create file class Customer and create class Artimatic

---
+++ OopCSharp/Customer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OopCSharp
+{
+    class Customer
+    {
+        // Member variables
+        public int CustID;
+        public string Name;
+        public string Gender;
+        public string Address;
+        public string NoPhone;
+
+        // Constructor for initializing fields
+        public Customer()
+        {
+            CustID = 001;
+            Name = "Agus Novian";
+            Gender = "Male";
+            Address = "Indoneia";
+            NoPhone = "+6281575541255";
+        }
+
+        // Method for displaying customer records (functionality)
+        public void displayData()
+        {
+            Console.WriteLine("Customer = " + CustID);
+            Console.WriteLine("Name = " + Name);
+            Console.WriteLine("Gender = " + Gender);
+            Console.WriteLine("Address = " + Address);
+            Console.WriteLine("No. Phone = " + NoPhone + "\n");
+        }
+    }
+}
--- OopCSharp/Program.cs
+++ OopCSharp/Program.cs
@@ -6,7 +6,32 @@ namespace OopCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("let's start coding OOP");
+            // Object instantiation
+            Customer customer = new Customer();
+
+            Aritmatic aritmatic = new Aritmatic();
+
+            // Method calling
+            customer.displayData();
+            
+            aritmatic.Additional();
+
+        }
+    }
+
+    class Aritmatic
+    {
+        // Variables
+        int x = 100;
+        int y = 200;
+        int z;
+
+        // Method
+        public void Additional()
+        {
+            z = x + y;
+            Console.WriteLine("Other class in Namespace");
+            Console.WriteLine(z);
         }
     }
 }
